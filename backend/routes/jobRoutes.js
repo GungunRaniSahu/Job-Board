e9d49d1e03d0d41ce4d5ec83287c3f94ec05553a
Gungun Sahu <gungunsahu1976@gmail.com>
2025-05-14 23:45:39 +0530

    added search bar and it's functionality

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/jobModel'); 
 
+// Create a new job
 router.post('/create', async (req, res) => {
   try {
     const { title, description, company, location, salary } = req.body;
@@ -21,9 +22,20 @@ router.post('/create', async (req, res) => {
   }
 });
 
+// Fetch jobs with pagination
 router.get('/', async (req, res) => {
   try {
-    const jobs = await Job.find();
+    // Extract page and limit from query parameters, with default values if not provided
+    const page = parseInt(req.query.page) || 1; // Default to page 1
+    const limit = parseInt(req.query.limit) || 5; // Default to 5 jobs per page
+    
+        const skip = (page - 1) * limit;
+
+    
+    const jobs = await Job.find()
+      .skip(skip)
+      .limit(limit);
+
     res.status(200).json(jobs);
   } catch (err) {
     res.status(400).json({ error: 'Error fetching jobs', details: err });
